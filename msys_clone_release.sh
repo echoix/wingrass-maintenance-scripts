a98fe0b91af5e4f8e976db399229c9672cb4a9f9
Edouard Choinière <27212526+echoix@users.noreply.github.com>
2026-02-08 15:22:32 -0500

    Change msys_clone_release for grass86, as only reference where grass86 folder could be created and cloned

--- msys_clone_release.sh
+++ msys_clone_release.sh
@@ -9,9 +9,9 @@ fi
 release=$1
 release_dir=`echo $release | sed 's/\.//g'`
 cd /usr/src
-(cd grass85; git pull)
+(cd grass86; git pull)
 rm -rf *RC* # remove all RC (clean-up)
-git clone grass85 grass$release_dir
+git clone grass86 grass$release_dir
 cd grass$release_dir
 git checkout $release
 git pull
